07573e58b4ab5b03dc032023437b202afe8b48e7
Vitaly <vs3rgienko@gmail.com>
2017-11-19 22:21:19 +0200

    some test with json file

--- wizz_api.py
+++ wizz_api.py
@@ -14,8 +14,9 @@ head = {'content-type': 'application/json'}
 #     'cookie': 'ASP.NET_SessionId=5fmps2oetzbge5qyubgwpawy; ak_bmsc=DA09A276B17E98B06D2869C6D9355167601194041A5300008CD80F5A17ACD105~plPPoQ420Bv9mGe/+GvLdyDFZMGrOOeYBpSXru5Q/TJTOX9ivOZy+byBzPVdijhjROmrB0t6U4nC/qw9A+uPioDgE99K0iqrFFk2ObqsLoXpv/pwsRgjRX1R34dIVz7Qu03gYl61STETB68OkWnfCSCYO5HMQtu33NZZ/MHvF6z5tKhYXWWQHAPOVHwWpHznmyXOKKARrcKHSp7zfYFjSFx2vM6/ZvHIJopbF6PoHh+kM=; bm_sv=596F572CF20263AF8A2D6B8CB908DA67~qFP3ykFhDLR8EmuGr0t8tF1cHxnkGhvdKPL5iGX0u+8RE9jGbknW2seSbsiCmbWrgSDsEg7t9cARFbUCzBQOiIA5TZ78s3bSzswL14waaFrB8VPn6LNQxhhsssaV1MvXN/H8Da8UgM8jIpt5usv92IZuJAbQfzE5jwF6uZk+Z/w=; _ga=GA1.2.1989400728.1510855716; _gid=GA1.2.594641519.1510855716; _gat=1'
 # }
 
-payload = {"flightList":[{"departureStation":"IEV","arrivalStation":"BUD","from":"2017-11-18","to":"2017-12-03"},{"departureStation":"BUD","arrivalStation":"IEV","from":"2017-11-27","to":"2017-12-31"}],"priceType":"regular"}
+payload = {"flightList":[{"departureStation":"IEV","arrivalStation":"BUD","from":"2017-11-19","to":"2017-12-03"},{"departureStation":"BUD","arrivalStation":"IEV","from":"2017-11-27","to":"2017-12-31"}],"priceType":"regular"}
 
 r = requests.post(request_url, headers=head, data=json.dumps(payload))
-
-print(r.content)
+c = r.content
+print(c)
+print(type(c))
